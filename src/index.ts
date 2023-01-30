e1115eb9692bbc0d089019bd6a9da7cbb922037c
SoraKumo <info@croud.jp>
2023-01-30 09:07:28 +0900

    update package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export const promiseLimit = (ps = new Set<Promise<unknown>>()) => ({
-  add: (p: Promise<unknown>) => ps.add(!!p.then(() => ps.delete(p)).catch(() => ps.delete(p)) && p),
+  add: (p: Promise<unknown> | (() => Promise<unknown>)) =>
+    ps.add(
+      typeof p === 'function' ? p() : !!p.then(() => ps.delete(p)).catch(() => ps.delete(p)) && p
+    ),
   wait: (limit: number) => ps.size >= limit && Promise.race(ps),
   all: () => Promise.all(ps),
 });
